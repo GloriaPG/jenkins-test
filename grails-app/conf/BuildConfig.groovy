a54ba340e655b0267b1fded089b20a004f93e4d7
Gloria Palma <ing.gloriapalmagonzalez@gmail.com>
2014-05-13 13:51:01 -0500

    poque es mi repor y hago push a master sin pedo

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -70,6 +70,4 @@ grails.project.dependency.resolution = {
         //runtime ":cached-resources:1.1"
         //runtime ":yui-minify-resources:0.1.5"
     }
-
-    //namas voy poner un comentario :P
 }
